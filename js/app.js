29746a2335d82cb894d121672eadb1b3ce058d8a
Thomas Sarkisian <tsarkisian@gmail.com>
2016-11-02 00:30:30 -0600

    Add: geo_locate.js (geolocation functionality) app.js (page loader/wait, html adjustments, gMaps header) files: marker.pngs, ajax-loader.gif

--- js/app.js
+++ js/app.js
@@ -1,41 +1,77 @@
 /* jQuery */
+'use strict';
 
 jQuery(document).ready(function($) {
-
-  function uwQuery() {
-    console.log("got uwQuery");
-    // http://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/IA/Cedar_Rapids.json
-    $.ajax({
-        url: "http://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/CO/Boulder.json",
-        dataType: "jsonp",
-        success: function(parsed_json) {
-            console.log(parsed_json);
-            var location = parsed_json['location']['city'];
-            var temp_f = parsed_json['current_observation']['temp_f'];
-            var alt_f = parsed_json['current_observation']['observation_location']['elevation'];
-            console.log(alt_f);
-            var alt_str = " Elevation: " + alt_f;
-            // alert("Current temperature in " + location + " is: " + temp_f);
-            var gotQuery = "Current temp in " + location + " " + "CO" + " is: " + temp_f+"F";
-            console.log(gotQuery);
-            $('#queryReturn').html(gotQuery + alt_str);
-        }
-    });
-  }
+    console.log("jQuery loaded...");
 
 
+    /* START: LOADING */
+    $(document).ajaxStop(function(){
+           console.debug("ajaxStop");
+           $("#ajax_loader").hide();
+        });
+    $(document).ajaxStart(function(){
+            console.debug("ajaxStart");
+            $("#ajax_loader").show();
+        });
+    /* STOP: LOADING */
 
-  function getSearchBoxText(e) {
-    e.preventDefault(); // Need this to prevent screen refresh ** READ UP ON TOPIC **
-    var queryStr = escape($("#search").val());
-    // var queryStr = $("#searchField").serialize(); /* with serialize() */
-    // var queryStr = {}
-    console.log("f:getSearchBoxText : ", queryStr);
-    // queryOMDB1(queryStr);
-    // queryOMDB2(queryStr);
-    uwQuery();
-  }
 
-  // Submit Button listener
-  $(function() {$("#submitButton").click(getSearchBoxText);});
+    // **** MOVED TO geo_locate:showPosition Callback ****
+    // Submit API query
+    function uwQuery(locationCords) {
+
+        console.log("f:uwQuery");
+        // http://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/IA/Cedar_Rapids.json
+        $.ajax({
+            // http://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/q/37.776289,-122.395234.json
+            // Fixed Boulder: https://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/CO/Boulder.json
+            url: "https://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/" + locationCords + ".json",
+            dataType: "jsonp",
+            success: function(parsed_json) {
+                $("#ajax_loader").hide();
+                console.log(parsed_json);
+                // var location = parsed_json['location']['city'];
+                var location = parsed_json['current_observation']['display_location']['full'];
+                var temp_f = parsed_json['current_observation']['temp_f'];
+                var alt_ft = parsed_json['current_observation']['observation_location']['elevation'];
+                // var gotQuery = "Current temp in " + location + " " + "CO" + " is: " + temp_f+"F";
+                var obloc = parsed_json['current_observation']['display_location']['state_name'];
+                console.log("query state_name", obloc);
+                $('#queryReturnLocation').html("Location : " + location);
+                $('#queryReturnElevation').html("Elevation : " + alt_ft);
+                $('#queryReturnTemp').html("Temperature : " + temp_f);
+                //getLocation();
+
+            }
+        });
+    }
+    // Get the City & State from text boxes
+    function getSearchBoxText(e) {
+        e.preventDefault(); // Need this to prevent screen refresh ** READ UP ON TOPIC **
+        var queryStr = escape($("#search").val());
+        // var queryStr = $("#searchField").serialize(); /* with serialize() */
+        // var queryStr = {}
+        console.log("f:getSearchBoxText : ", queryStr);
+        // queryOMDB1(queryStr);
+        // queryOMDB2(queryStr);
+        // uwQuery();
+        $("#ajax_loader").show();
+        getLocation(); // bypass textbox input of location
+    }
+
+    // Submit Button listener : Method 1 ******************
+    $("#submitButton").on({ "click": getSearchBoxText});
+    // ****************************************************
+    // Submit Button listener : Method 2 ******************
+    // var submitBtn = $("#submitButton");
+    // submitBtn.click( getSearchBoxText);
+    // ****************************************************
+
+
+    google.maps.event.addDomListener(window, 'load', initHeaderMap);
+    // End : Map Header
+
+
+
 });
---
+++ js/geo_locate.js
@@ -0,0 +1,164 @@
+'use strict';
+
+var x = document.getElementById("geocoords");
+
+function getLocation() {
+  console.log("f:getLocation");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+
+        // uwQuery(latlon);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    // START : temp
+    $.ajax({
+      // http://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/q/37.776289,-122.395234.json
+      // Fixed Boulder: https://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/CO/Boulder.json
+        url: "https://api.wunderground.com/api/425f5dcedcd8dbce/geolookup/conditions/q/"+latlon+".json",
+        dataType: "jsonp",
+        success: function(parsed_json) {
+            console.log(parsed_json);
+            // var location = parsed_json['location']['city'];
+            var location = parsed_json['current_observation']['display_location']['full'];
+            var temp_f = parsed_json['current_observation']['temp_f'];
+            var alt_ft = parsed_json['current_observation']['observation_location']['elevation'];
+            var icon_url = parsed_json['current_observation']['icon_url'];
+            // var gotQuery = "Current temp in " + location + " " + "CO" + " is: " + temp_f+"F";
+            var obloc = parsed_json['current_observation']['display_location']['state_name'];
+            console.log("query state_name", obloc);
+            $('#queryReturnLocation').html("Location : " + location);
+            $('#queryReturnElevation').html("Elevation : " + alt_ft);
+            $('#queryReturnTemp').html("Temperature : " + temp_f);
+            document.getElementById("forecastIcon").innerHTML = "<img src='"+icon_url+"'>"; //  make img-responsive
+
+            //Satellite
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            var satMap = "https://api.wunderground.com/api/425f5dcedcd8dbce/satellite/image.gif?lat=+lat+&lon=+lon+&radius=100&width=400&height=300&key=sat_ir4_bottom&basemap=1";
+
+            /*"http://api.wunderground.com/api/425f5dcedcd8dbce/satellite/image.gif?lat=38&lon=-96.4&radius=100&width=280&height=280&key=sat_ir4_bottom&basemap=1";
+            */
+            var satMapClass = document.getElementById("satShow");
+            satMapClass.className = "img-responsive";
+            document.getElementById("satShow").innerHTML = "<img src='"+satMap+"'>";
+            // var map;
+            // initMap(map, lat, lon);
+            var zoomLev = 16;
+            initMap(lat,lon,zoomLev);
+
+        }
+    });
+    // END : temp AIzaSyANnyMltzBRGiRZs1FoOTf-asFZBDkmprc
+    //
+    // var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
+    // document.getElementById("mapShow").innerHTML = "<img src='"+img_url+"'>";
+    // console.log("f:showLocation : latlon : ", latlon);
+
+
+}
+
+// var map;
+      function initMap(lat, lon, zoomLev) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: lat, lng: lon},
+          zoom: zoomLev
+        });
+
+        var markerImage = 'marker2.png';
+
+        var marker = new google.maps.Marker({
+            position: {lat: lat, lng: lon},
+            map: map,
+            icon: markerImage,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+        map.set('styles', styles);
+      }
+
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
+// START : Map Header
+function initHeaderMap() {
+    // var location = new google.maps.LatLng(50.0875726, 14.4189987);
+    var location = new google.maps.LatLng(37.09024, -95.712891);
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: location,
+        zoom: 4,
+        panControl: false,
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    // var markerImage = 'marker.png';
+    //
+    // var marker = new google.maps.Marker({
+    //     position: location,
+    //     map: map,
+    //     icon: markerImage
+    // });
+
+    // var contentString = '<div class="info-window">' +
+    //         '<h3>Info Window Content</h3>' +
+    //         '<div class="info-content">' +
+    //         '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+    //         '</div>' +
+    //         '</div>';
+    //
+    // var infowindow = new google.maps.InfoWindow({
+    //     content: contentString,
+    //     maxWidth: 400
+    // });
+
+    // marker.addListener('click', function () {
+    //    infowindow.open(map,marker);
+    // });
+
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+    map.set('styles', styles);
+
+
+}
+
+/*
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+*/
